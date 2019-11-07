6fcd9e57b8473b324055032115da797a6d309c16
Elizabeth Silverstein <elizabethAsilverstein@gmail.com>
2019-11-07 16:49:52 -0600

    All restaurants ready

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import MainContainer from './mainContainer'
-import {fakeData, addToWaitlist, getFullWaitlist, cancelReservation, nextParty, howLongIsMyWait} from './DatabaseCalls'
+import {fakeData, addToWaitlist, getFullWaitlist, cancelReservation, nextParty, howLongIsMyWait, allRestaurants} from './DatabaseCalls'
 
 class App extends Component{
 
@@ -11,13 +11,14 @@ class App extends Component{
   }
   render(){
   //fakeData();
-  addToWaitlist({guestPhoneNumber: 2222222, partySize: 1}, "BrightWok")
+  //addToWaitlist({guestPhoneNumber: 2222222, partySize: 1}, "BrightWok")
   //You need to await the response from getFullWaitlist
   //getFullWaitlist("Bombay Wraps")
   //cancelReservation(2, "BrightWok")
   //nextParty(1, "BrightWok")
   //Returns how long your wait is!
   //howLongIsMyWait(2222222, "BrightWok")
+  //allRestaurants()
     return (
       <div className="App">
         <MainContainer />
