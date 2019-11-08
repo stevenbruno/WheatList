b0f2f2f7d306d07ded19264438329b3867dd7f70
Steven Bruno <sbruno636@gmail.com>
2019-11-07 20:21:00 -0600

    add the color splash header

--- src/Components/restaurantBanner.js
+++ src/Components/restaurantBanner.js
@@ -5,7 +5,6 @@ import '../App.css'
 const RestaurantBanner = (props) => {
         return(
             <div className="restaurantBanner">
-                {/* <img className="restaurantImage" src={props.image}></img> */}
                 <h1 className="restaurantHeading">{props.name}</h1>
             </div>
 
