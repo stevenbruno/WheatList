303e5e47169782a6780fcba6e81395952eb08a3d
Steven Bruno <sbruno636@gmail.com>
2019-11-07 20:38:14 -0600

    attempt icon reference

--- src/Components/restaurantBanner.js
+++ src/Components/restaurantBanner.js
@@ -5,6 +5,7 @@ import '../App.css'
 const RestaurantBanner = (props) => {
         return(
             <div className="restaurantBanner">
+                {/* <img src={props.image} alt="wok icon" /> */}
                 <h1 className="restaurantHeading">{props.name}</h1>
             </div>
 
